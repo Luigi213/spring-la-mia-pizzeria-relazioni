016f74dfadc923b6eb0caede69b8da8e7000d9d4
Luigi213 <reyesgianluigi88@gmail.com>
2023-06-01 10:20:32 +0200

    optimized order file

--- src/main/java/org/java/project/controller/PizzaController.java
+++ src/main/java/org/java/project/controller/PizzaController.java
@@ -3,10 +3,7 @@ package org.java.project.controller;
 import java.util.List;
 import java.util.Optional;
 
-import org.hibernate.query.QueryParameter;
-import org.java.project.pojo.Offerte;
 import org.java.project.pojo.Pizza;
-import org.java.project.serv.ServiceOfferte;
 import org.java.project.serv.ServicePizza;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -19,8 +16,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.mysql.cj.Query;
-
 import jakarta.validation.Valid;
 
 @Controller
@@ -28,9 +23,6 @@ import jakarta.validation.Valid;
 public class PizzaController {
 	@Autowired
 	private ServicePizza servicePizza;
-	
-	@Autowired
-	private ServiceOfferte serviceOfferte;
 	
 	@GetMapping("/")
 	public String getHome(Model model) {
@@ -59,80 +51,6 @@ public class PizzaController {
 		return "pizza";
 	}
 	
-	@GetMapping("/new-offerte/create/{id}")
-	public String createNewOfferta( 
-			Model model,
-			@PathVariable("id") int id
-		) {
-		
-		List<Pizza> p = servicePizza.findAll();
-		
-		model.addAttribute("pizza", p);
-		model.addAttribute("id", id);
-		model.addAttribute("offerte", new Offerte());
-		
-		return "new-offerte";
-	}
-	
-	@PostMapping("/new-offerte/create")
-	public String storeNewOfferta(
-			Model model,
-			@Valid @ModelAttribute Offerte offerte,
-			BindingResult bindingResult) {
-		
-		if (bindingResult.hasErrors()) {
-			
-			model.addAttribute("offerte", offerte);
-			model.addAttribute("errors", bindingResult);
-			model.addAttribute("id", offerte.getPizza().getId());
-			
-			return "new-offerte";
-		}
-		
-		
-		serviceOfferte.save(offerte);
-		
-		return "redirect:/pizza/" + offerte.getPizza().getId();
-	}
-	
-	
-	@GetMapping("/new-offerte/update/{id}")
-	public String editOfferte(
-			Model model,
-			@PathVariable("id") int id
-		) {
-		
-		List<Pizza> p = servicePizza.findAll();
-		model.addAttribute("pizz", p);
-		
-		Optional<Offerte> optPizza = serviceOfferte.findById(id);
-		Offerte offerte = optPizza.get();
-		
-		model.addAttribute("offerte", offerte);
-		
-		return "offerte-update";
-	}
-	
-	
-	@PostMapping("/new-offerte/update/{id}")
-	public String updateOfferte(
-			Model model,
-			@Valid @ModelAttribute Offerte offerte,
-			BindingResult bindingResult
-		) {
-		
-		if (bindingResult.hasErrors()) {
-			
-			model.addAttribute("offerte", offerte);
-			model.addAttribute("errors", bindingResult);
-			
-			return "offerte-update";
-		}
-		
-		serviceOfferte.save(offerte);
-		
-		return "redirect:/pizza/" + offerte.getPizza().getId();
-	}
 	
 	@PostMapping("/pizza/by/name")
 	public String getNamePizza(Model model, @RequestParam(required = false) String name) {
@@ -173,21 +91,6 @@ public class PizzaController {
 		return "redirect:/";
 	}
 	
-	
-	@GetMapping("/pizza/delete/{id}")
-	public String deletePizza(
-			@PathVariable("id") int id
-		) {
-		
-		Optional<Pizza> optPizza = servicePizza.findById(id);
-		Pizza pizza = optPizza.get();
-		
-		servicePizza.delete(pizza);
-		
-		return "redirect:/";
-	}
-	
-	
 	@GetMapping("/pizza/update/{id}")
 	public String editPizza(
 			Model model,
@@ -201,7 +104,6 @@ public class PizzaController {
 		
 		return "pizza-update";
 	}
-	
 	
 	@PostMapping("/pizza/update/{id}")
 	public String updatePizza(
@@ -219,6 +121,19 @@ public class PizzaController {
 		}
 		
 		servicePizza.save(pizza);
+		
+		return "redirect:/";
+	}
+	
+	@GetMapping("/pizza/delete/{id}")
+	public String deletePizza(
+			@PathVariable("id") int id
+			) {
+		
+		Optional<Pizza> optPizza = servicePizza.findById(id);
+		Pizza pizza = optPizza.get();
+		
+		servicePizza.delete(pizza);
 		
 		return "redirect:/";
 	}
